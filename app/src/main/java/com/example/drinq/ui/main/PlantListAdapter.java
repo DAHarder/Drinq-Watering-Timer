fabce911887f2ae584c4b38f43afd34fdbc29252
Dan Adams <danadamsharder@gmail.com>
2021-07-18 22:49:11 -0700

    Added time until next water on each plant item in recycler view

--- app/src/main/java/com/example/drinq/ui/main/PlantListAdapter.java
+++ app/src/main/java/com/example/drinq/ui/main/PlantListAdapter.java
@@ -3,6 +3,7 @@ package com.example.drinq.ui.main;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +28,6 @@ import java.time.temporal.ChronoUnit;
 public class PlantListAdapter extends ListAdapter<PlantEntity, PlantListAdapter.PlantListViewHolder> {
 
     private final Context context;
-    long wateredDateDiff;
 
     public PlantListAdapter(@NonNull DiffUtil.ItemCallback<PlantEntity> diffCallback, Context context) {
         super(diffCallback);
@@ -44,10 +44,8 @@ public class PlantListAdapter extends ListAdapter<PlantEntity, PlantListAdapter.
     @Override
     public void onBindViewHolder(@NonNull PlantListViewHolder holder, int position) {
         PlantEntity current = getItem(position);
-        if (PlantWaterNotice.plantWaterNotice(current))
-            holder.bind(current.getPlantName(), current.getPlantDescription(), true);
-        else
-            holder.bind(current.getPlantName(), current.getPlantDescription(),false);
+        holder.bind(current);
+
     }
 
     static class PlantDiff extends DiffUtil.ItemCallback<PlantEntity> {
@@ -68,12 +66,14 @@ class PlantListViewHolder extends RecyclerView.ViewHolder {
         private final TextView plantItemView;
         private final TextView plantDescriptionView;
         private final ImageView plantNeedsWaterIcon;
+        private final TextView plantWatertime;
 
         private PlantListViewHolder(View itemView) {
             super(itemView);
             plantItemView = itemView.findViewById(R.id.plant_item_text_view);
             plantDescriptionView = itemView.findViewById(R.id.plant_item_description_view);
             plantNeedsWaterIcon = itemView.findViewById(R.id.water_needed_icon);
+            plantWatertime = itemView.findViewById(R.id.water_days_left);
 
             itemView.setOnClickListener(new View.OnClickListener() {
 
@@ -89,16 +89,21 @@ class PlantListViewHolder extends RecyclerView.ViewHolder {
 
         }
 
-        public void bind(String plantName, String plantDescription, boolean icon) {
-            plantItemView.setText(plantName);
+        public void bind(PlantEntity plant) {
+            plantItemView.setText(plant.getPlantName());
+            plantWatertime.setText(String.valueOf(PlantWaterNotice.getPlantWaterTime(plant) + " days"));
 
-            if (plantDescription != null)
-            plantDescriptionView.setText(plantDescription);
-
-            if (!icon)
-                plantNeedsWaterIcon.setVisibility(View.GONE);
+            if (PlantWaterNotice.getPlantWaterTime(plant) <= 0)
+                plantWatertime.setTextColor(Color.RED);
             else
+                plantWatertime.setTextColor(Color.BLACK);
+
+            if (plant.getPlantDescription() != null)
+            plantDescriptionView.setText(plant.getPlantDescription());
+            if (PlantWaterNotice.plantWaterNotice(plant))
                 plantNeedsWaterIcon.setVisibility(View.VISIBLE);
+            else
+                plantNeedsWaterIcon.setVisibility(View.GONE);
         }
     }
 
--- app/src/main/java/com/example/drinq/util/PlantWaterNotice.java
+++ app/src/main/java/com/example/drinq/util/PlantWaterNotice.java
@@ -8,12 +8,19 @@ import java.time.temporal.ChronoUnit;
 public class PlantWaterNotice {
 
     static long wateredDateDiff;
+    static long plantTimeUntilWater;
 
     static public boolean plantWaterNotice(PlantEntity plant) {
     wateredDateDiff = ChronoUnit.DAYS.between(LocalDate.parse(plant.getLastWateredDate()), LocalDate.now());
-        if (wateredDateDiff + 1 >= plant.getWateringInterval())
+        if (wateredDateDiff >= plant.getWateringInterval())
                 return true;
         else
                 return false;
 }
+    static public long getPlantWaterTime(PlantEntity plant){
+        wateredDateDiff = ChronoUnit.DAYS.between(LocalDate.parse(plant.getLastWateredDate()), LocalDate.now());
+        plantTimeUntilWater = plant.getWateringInterval() - wateredDateDiff;
+        return plantTimeUntilWater;
+    }
+
 }
